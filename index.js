2147cc91d4c1ce64dc45aaa8731d5a234cd418d9
Kevin Okamoto <kevin.okamoto@sap.com>
2021-06-09 10:10:46 -0700

    Adding code to parse POST body and save to DB

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const bodyParser = require('body-parser');
+
 /**
  * Set up Mongoose
  */
@@ -13,7 +15,7 @@ const mongodbUrl = 'mongodb://mongo:27017/mockrestapi';
 
 mongoose.connect(mongodbUrl, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error 😢'));
+db.on('error', console.error.bind(console, 'MongoDB connection error 😢'));
 db.once('open', function() {
     console.log('Connected to DB 🚀');
 });
@@ -24,38 +26,52 @@ db.once('open', function() {
 const responseSchema = new mongoose.Schema({
     key: String,
     method: String,
-    header: String,
+    headers: String,
     body: String,
 });
 const Response = mongoose.model('Response', responseSchema);
 
-app.get('/_intro_', (req, res) => {
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 app.post('/_save_', (req, res) => {
     const data = req.body;
+    if (!data.key) {
+        return res.status(400).send('Missing attribute: "key"');
+    }
+    if (!data.body) {
+        return res.status(400).send('Missing attribute: "body"');
+    }
     const response = new Response({
-        key: data.path,
+        key: data.key,
         method: data.method,
-        header: null,
+        headers: data.headers ? JSON.stringify(data.headers): null,
         body: JSON.stringify(data.body)
     });
     response.save((err, response) => {
        if (err) return console.error(err);
-       res.send('Saved entry');
+       res.send('Entry saved');
     });
 });
 
 app.all('*', (req, res, next) => {
-    Response.find({
+    Response.findOne({
         key: req.path
-    }, (response) => {
-        if (response) {
+    }, (err, response) => {
+        if (err) {
+            console.error(err);
+            res.status(400).send('System error');
+        } else if (!response || !response.body) {
+            console.log('Not found: ' + req.path);
+            res.status(404).send('Not found');
+        } else  {
             res.send(JSON.parse(response.body));
         }
+        next();
     });
-    next();
 });
 
 app.listen(port, () => {
